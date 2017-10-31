69aad2ecb6409b84f88f2eadbf76d6bc4d79ad91
novice <mandisi.makwakwa@gmail.com>
2017-10-31 22:15:37 +0200

    ft_printf_update

---
+++ ft_misc2.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_misc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakwakw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/03 15:39:53 by mmakwakw          #+#    #+#             */
+/*   Updated: 2017/01/03 15:42:46 by mmakwakw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ft_printf.h"
+
+int     ft_getWidth(char *format, char flag)
+{
+
+    //Check whats before the specifier
+    //Get the value if there is one
+    //return result
+}
