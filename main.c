7f4c748d63ab6ba2ddce4e974f2ea449bc1df1ed
mandisimakwakwa <mandisi.makwakwa@gmail.com>
2017-11-27 16:05:01 +0200

    ft_printf_update

--- main.c
+++ main.c
@@ -758,5 +758,17 @@ int		main(void)
     ft_printf("Input\t\t: &tmpInt\n");
     ft_printf("Output\t\t: ");
     ft_printf("%p\n\n", &tmpInt);
+
+    //Specifier 10p check
+    printf("Test 59\t\t: 10p\n\n");
+    printf("Function\t: Printf\n");
+    printf("Input\t\t: &tmpInt\n");
+    printf("Output\t\t: ");
+    printf("%10p\n\n", &tmpInt);
+
+    ft_printf("Function\t: ft_printf\n");
+    ft_printf("Input\t\t: &tmpInt\n");
+    ft_printf("Output\t\t: ");
+    ft_printf("%10p\n\n", &tmpInt);
     return (0);
 }
